721ff44ec282ab24ad5bd3a0ce7e47b4e2485683
mtoner23 <mtoner@purdue.edu>
2017-12-02 18:12:48 -0500

    Made a test bench

---
+++ source/tb_USB_tx_top_level.sv
@@ -0,0 +1,13 @@
+// $Id: $
+// File name:   tb_USB_tx_top_level.s
+// Created:     12/2/2017
+// Author:      Michael Toner
+// Lab Section: 337-05
+// Version:     1.0  Initial Design Entry
+// Description: Top Level Test bench for USB TX
+module tb_USB_tx_top_level
+
+
+
+
+endmodule
