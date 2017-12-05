293ae56bbe65315c2e427e21fd90c182e3447e81
ybharatu <ybharatu@purdue.edu>
2017-12-05 01:04:44 -0500

    Created main controller file

---
+++ source/main_controller.sv
@@ -0,0 +1,16 @@
+// $Id: $
+// File name:   main_controller.sv
+// Created:     12/5/2017
+// Author:      Yashwanth Bharatula
+// Lab Section: 337-05
+// Version:     1.0  Initial Design Entry
+// Description: Main State Machine
+module main_controller
+(
+	input host_ready,
+	input rcv_error,
+	output quit_hash,
+)
+
+
+endmodule
