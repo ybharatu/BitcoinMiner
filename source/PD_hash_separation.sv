80d1cd85e258e0aa480e197e188c6de9e70b7e7f
Rahul Patni <mg67@purdue.edu>
2017-12-06 02:25:51 -0500

    fixing hash separation with test becnh

--- source/PD_hash_separation.sv
+++ source/PD_hash_separation.sv
@@ -9,34 +9,83 @@
 module PD_hash_separation
 (
 	input wire clk, n_rst,
-	input wire [15:0][15:0] hash,
-	input wire write_fifo,
-	output wire [15:0] write_data
+	input wire [17:0][15:0] hash, // SYNC+PID, METADATA, HASH+NONCE
+	input wire transmit_empty,
+	input wire transmit_empty_en,
+	input wire [7:0] PID,
+	input wire PID_en,
+	input wire read_enable, // changed from write_fifio
+	output wire [15:0] tx_data // changed from write_enable
 );
 
 localparam [7:0] sync_byte = 8'b01010100;
-localparam [7:0] PID = 8'b00000000; // Need to change
+// localparam [7:0] PID = 8'b00000000; // Need to change
 
 logic [15:0] check_write_data;
-assign write_data = check_write_data;
+assign tx_data = check_write_data;
 
 logic [4:0] count_out;
 
-flex_counter_fix #(5) FLEX_COUNTER_FIX (.clear(), .count_enable(write_fifo), .clk(clk), .n_rst(n_rst), .rollover_val(5'd16), .rollover_flag(), .count_out(count_out));
+reg transmit_empty_reg;
+reg [7:0] PID_reg;
 
-// assign write_data = (count_out < 16) ? hash[count_out] : {sync_byte, PID};
+always_ff @(posedge clk, negedge n_rst) begin
+	if (n_rst == 0) begin
+		transmit_empty_reg <= '0;
+	end
+	else if (transmit_empty_en == 1'b1) begin
+		transmit_empty_reg <= transmit_empty;
+	end
+	else begin
+		transmit_empty_reg <= transmit_empty_reg;
+	end 
+end
+
+always_ff @(posedge clk, negedge n_rst) begin
+	if (n_rst == 0) begin
+		PID_reg <= '0;
+	end
+	else if (PID_en == 1'b1) begin 
+		PID_reg <= PID;
+	end 
+	else begin
+		PID_reg <= PID_reg;
+	end
+end
+
+flex_counter_fix #(5) FLEX_COUNTER_FIX (.clear(), .count_enable(read_enable), .clk(clk), .n_rst(n_rst), .rollover_val(5'd19), .rollover_flag(), .count_out(count_out));
 
 always_comb begin
 	integer j;
 	check_write_data = '0;
-	for (j = 0; j < 17; j = j + 1) begin
-		if (j == count_out && j < 16) begin
-			check_write_data = hash[j];
+
+	for (j = 0; j < 20; j = j + 1) begin
+		if (j == 0 && j == count_out) begin	
+			check_write_data = {sync_byte, PID_reg};
 		end
-		else if (count_out == j && j == 16) begin
-			check_write_data = {sync_byte, PID};
+		else if (j == 1 && j == count_out) begin
+			if (transmit_empty_reg == 1'b0) begin
+				check_write_data = 16'd0;
+			end
+			else begin
+				check_write_data = 16'd1;
+			end
 		end
+		else if (j == count_out) begin
+			check_write_data = hash[17-(j-2)];
+		end
+		
 	end
 end 
 
 endmodule
+
+/*
+	for (j = 0; j < 17; j = j + 1) begin
+		if (j == count_out && j < 18) begin
+			check_write_data = hash[j];
+		end
+		else if (count_out == j && j == 18) begin
+			check_write_data = {sync_byte, PID_reg};
+		end
+*/
--- source/tb_PD_hash_separation.sv
+++ source/tb_PD_hash_separation.sv
@@ -30,9 +30,15 @@ module tb_PD_hash_separation ();
 	integer tb_test_num = 0;
 
 	reg tb_n_rst;
-	reg [15:0][15:0]tb_hash;
-	reg tb_write_fifo;
-	reg [15:0]tb_write_data; 
+
+	reg [17:0][15:0]tb_hash;
+	reg tb_transmit_empty;
+	reg tb_transmit_empty_en;	
+	reg [7:0]tb_PID;
+	reg tb_PID_en;
+	reg tb_read_enable;
+	reg [15:0]tb_tx_data;
+
 	integer i;
 
 	clocking cb @(posedge tb_clk);
@@ -41,40 +47,74 @@ module tb_PD_hash_separation ();
 		output #800ps n_rst = tb_n_rst; // FIXME: Removal time (01CLK -> 01R) is 281.25ps, but this needs to be 800 to prevent metastable value warnings
 		output  n_rst = tb_n_rst,
 			hash = tb_hash,
-			write_fifo = tb_write_fifo;
-		input	write_data = tb_write_data;
+			transmit_empty = tb_transmit_empty,
+			transmit_empty_en = tb_transmit_empty_en,
+			PID = tb_PID,
+			PID_en = tb_PID_en,
+			read_enable = tb_read_enable;
+		input	tx_data = tb_tx_data;
 	endclocking
 
-	PD_hash_separation DUT (.clk(tb_clk), .n_rst(tb_n_rst), .hash(tb_hash), .write_fifo(tb_write_fifo), .write_data(tb_write_data));
+	PD_hash_separation DUT (.clk(tb_clk), .n_rst(tb_n_rst), .hash(tb_hash), .transmit_empty(tb_transmit_empty), .transmit_empty_en(tb_transmit_empty_en),
+				.PID(tb_PID), .PID_en(tb_PID_en), .read_enable(tb_read_enable),	.tx_data(tb_tx_data));
 
 	initial
 	begin
+		//@cb;
+		tb_read_enable <= 1'b0;
+		@cb;
 		tb_n_rst = 1'b0;
-		tb_hash =  {32'hf20015ad,32'hb410ff61,32'h96177a9c,32'hb00361a3,
-				32'h5dae2223,32'h414140de,32'h8f01cfea,32'hba7816bf};
-		tb_write_data <= '0;
-		tb_write_fifo <= 1'b0;
 		@cb;
 		tb_n_rst = 1'b1;
+
+		//@cb;
 		@cb;
-	
-		tb_write_fifo <= 1'b1;
-		for(i = 0; i < 17; i = i + 1) begin
+
+		tb_hash =  {32'hf20015ad,32'hb410ff61,32'h96177a9c,32'hb00361a3,
+				32'h5dae2223,32'h414140de,32'h8f01cfea,32'hba7816bf,32'hb5016bf};
+
+		tb_tx_data <= '0;
+		tb_PID <= 8'b11101100;
+		tb_transmit_empty <= 1'b1;
+		tb_read_enable <= 1'b0;
+		tb_transmit_empty_en <= 1'b0;
+		tb_PID_en <= 1'b0;
+		@cb;
+		@cb;
+		@cb;
+		tb_transmit_empty_en <= 1'b1;
+		tb_PID_en <= 1'b1;
+		@cb;
+		tb_transmit_empty_en <= 1'b0;
+		tb_PID_en <= 1'b0;
+		
+		//@cb;
+		#CHECK_DELAY;	
+		tb_read_enable <= 1'b1;
+//		@cb;
+		for(i = 0; i < 20; i = i + 1) begin
 			tb_test_num = tb_test_num + 1;
-			#CHECK_DELAY;	 	
-			if (i < 16) begin
-				if (tb_write_data == tb_hash[i])
+			#CHECK_DELAY;	 
+			if (i == 0) begin
+				if ({8'b01010100, tb_PID} == tb_tx_data)
+					$info("Test Case %d Passed SYNC",tb_test_num);
+				else
+					$error("Test Case %d Failed SYNC",tb_test_num);	
+			end	
+			else if (i == 1) begin
+				if ({16'd1} == tb_tx_data)
+					$info("Test Case %d Passed TRANSMIT",tb_test_num);
+				else
+					$error("Test Case %d Failed TRANSMIT",tb_test_num);	
+			
+			end
+			else if (i < 20) begin
+				if (tb_tx_data == tb_hash[17-(i-2)])
 					$info("Test Case %d Passed REG",tb_test_num);
 				else
 					$info("Test Case %d Failed REG",tb_test_num);				
 				
 			end
-			else begin
-				if (tb_write_data == 16'b0101010000000000)
-					$info("Test Case %d Passed SYNC",tb_test_num);
-				else
-					$info("Test Case %d Failed SYNC",tb_test_num);				
-			end	
 			@cb;
 			#CHECK_DELAY;		
 		end
