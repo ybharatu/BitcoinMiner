fed2b8951e7f1542db982f4c75a9db029ab93ed6
Rahul Patni <mg67@purdue.edu>
2017-12-06 02:25:06 -0500

    incomplete top level

--- source/bitcoin_miner.sv
+++ source/bitcoin_miner.sv
@@ -9,12 +9,38 @@ module bitcoin_miner
 (
 	input clk,
 	input n_rst,
-	inout d_plus,
-	inout d_minus,
+	inout wire d_plus,
+	inout wire d_minus
 );
-	
-	PD_top_level PD (.clk(clk), .n_rst(n_rst), .chunk_1(chunk_1), .chunk_2(chunk_2), )
 
+logic transmit_ack;
+logic write_enable;
+logic rcv_error;
+logic eop;
+logic [7:0] rx_data;
+logic read_enable;
+logic [15:0] tx_data;
+logic transmit_empty;
+logic transmit_start;
+
+US_rx_top_level USB_RX (.clk(clk), .n_rst(n_rst), .d_plus_in(d_plus), .d_minus_in(d_minus), .packet_type(packet_type), .transmit_ack(transmit_ack), // INPUTS
+			.rx_data(rx_data), .write_enable(write_enable), .rcv_error(rcv_error), .eop(eop)); // OUTPUTS
+
+US_tx_top_level USB_TX (.clk(clk), .n_rst(n_rst), .tx_data(tx_data), .transmit_empty(transmit_empty), .transmit_start(transmit_start), // INPUTS
+			.d_plus_out(d_plus), .d_minus_out(d_minus), .read_enable(read_enable), ); // OUTPUTS
+
+PD_hash_separation HASH_SEPARATION (.clk(clk), .n_rst(n_rst));
+
+
+
+
+main_controller MAIN_CONTROLLER (.clk(clk), .n_rst(n_rst));
+HM_top_level HM_1 (.clk(clk), .n_rst(n_rst));
+
+PD_top_level PD (.clk(clk), .n_rst(n_rst), .chunk_1(chunk_1), .chunk_2(chunk_2), )
 
 endmodule
 
+
+// Things to figure out
+// packet_type
